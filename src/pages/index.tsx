fc470be043a044c31c6a3faba9d431528b92818e
Tonozaki <106017628+kakikuu@users.noreply.github.com>
2023-08-24 19:16:18 +0900

    8/24 Home画面更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,32 @@
-import type { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
-import Head from "next/head";
-import styles from "@/styles/Home.module.css";
-import { buildClient, IPostFields } from "@/lib/contentful";
-import Link from "next/link";
-import { Entry, EntryCollection } from "contentful";
+import styles from '../styles/boh_home.module.css'
 
-const client = buildClient();
+export default function about() {
+    return(
+        <div className={styles.AllPage}>
+            <main>
+                <section className={styles.section} id={styles.section1}>
+                    <img src="img/home_image.png" className={styles.Page1_image} />
+                    <div className={styles.Page1}>
+                        <h1 className={styles.TitleName}>BohPJ</h1>
+                        <h2 className={styles.UniversityName}>Musashino University</h2>
+                    </div>
+                </section>
 
-export const getStaticProps: GetStaticProps = async () => {
-  const { items }: EntryCollection<IPostFields> = await client.getEntries({
-    content_type: "post",
-    order: "-sys.createdAt",
-  });
-  return {
-    props: { posts: items },
-  };
-};
+                <section className={styles.section} id={styles.section2}>
+                    <div className={styles.image}></div>
+                    <div className={styles.Page2}>
+                        <h1 className={styles.About}>About</h1>
+                        <p className={styles.Introduction}>本プロジェクトは、2019年5月から大学の講義とは別に有志の学生が集まり、週1回のミーティングを行いながら研究(及び開発)を進めるプロジェクトです。 これまでに、画像認識や自然言語処理を用いた研究、またこれらの研究成果を取り込んだアプリケーション実装などを学生主体で実施し、オープンキャンパスや学会などで成果の発表を行ってきました。 データサイエンスやプログラムに関する技術や知識が今はまだないの人であっても、本プロジェクトでの活動で、大学の学習ペースに囚われることなく、学びたいことを追求していくことができます。</p>
+                    </div>
+                </section>
 
-type Props = InferGetStaticPropsType<typeof getStaticProps>;
-
-const Home: NextPage<Props> = ({ posts }) => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>BohPJ</title>
-      </Head>
-      <main className={styles.main}>
-        <div>
-            <h1>BohPJとは</h1>
-            <ul className={styles.flex}>
-                <li>aaa</li>
-                <li>bbb</li>
-            </ul>
+                <section className={styles.section} id={styles.section3}> 
+                    <div className={styles.image}></div>
+                    <div className={styles.Page3}>
+                        <h1 className={styles.News}>News</h1>
+                    </div>
+                </section>
+            </main>
         </div>
-      </main>
+    )
+}
