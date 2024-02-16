a456727c8ee3e0dbc44e3a0e2d37d56a19eefff6
kakikuu <s2222055@stu.musashino-u.ac.jp>
2024-02-17 00:39:10 +0900

    fix:投稿のテキストが表示されない問題の解決

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,51 +1,41 @@
-import Image from 'next/image'
-import Link from 'next/link'
+// WorksPage コンポーネント
+import Footer from "../components/footer";
+import Navbar from "../components/navigation";
+import { fetchAsset } from "../libs/client_asset";
+import { fetchEntries } from "../libs/client_entries";
+import DisplayContents from "../components/works/contents/index";
+import WorksHeader from "../components/works/worksHeader/index";
+import { WorksPosts } from "@/types/works";
 
-import { Works } from "@/types/works";
-import Footer from '../components/footer';
-import Navbar from '../components/navigation';
-import { fetchEntries } from '../libs/client_entries'
-import { fetchAsset } from '../libs/client_asset'
-import DisplayContents from '../components/works/contents/index'
-import WorksHeader from '../components/works/worksHeader/index'
+type WorksComponentProps = {
+  posts: WorksPosts[];
+};
 
-
-// Postsを定義している。propsとして、worksを受け取る
-//　下のは関数を定義している(関数としてコンポーネントを定義している)
-const WorksPage = ({ posts, assets }) => {
+const WorksPage: React.FC<WorksComponentProps> = ({ posts }) => {
   return (
     <div>
       <main>
         <Navbar></Navbar>
         <WorksHeader></WorksHeader>
-        <DisplayContents assets={assets} posts={posts} />
+        <DisplayContents posts={posts} />
         <Footer></Footer>
       </main>
-
     </div>
   );
-}
+};
 
 export default WorksPage;
 
-// getStaticPropsの定義
 export async function getStaticProps() {
+  const resEntries = await fetchEntries(); // 投稿されているデータをすべて取得
 
-  const resEntries = await fetchEntries() // タイトルや概要を取得
-  const resAssets = await fetchAsset() //スライドのデータを取得
+  const posts = resEntries.map((p) => {
+    return p.fields;
+  });
 
-  const posts = await resEntries.map((p) => {
-    return p.fields
-  })
-  const assets = await resAssets.Asset.map((a) => {
-    return a.fields
-
-  })
   return {
     props: {
-      posts, // fetched data
-      assets, // fetched data
+      posts,
     },
-  }
+  };
 }
-
