bde85c06de3bbec941db65656c684c17ccb71662
hirotakagi <s2222103@stu.musashino-u.ac.jp>
2024-02-17 01:11:17 +0900

    画像を表示されるように修正

--- src/data/members.tsx
+++ src/data/members.tsx
@@ -6,248 +6,297 @@ const membersData = [
   {
     classId: 1,
     name: "あべゆー",
-    image: "https://drive.google.com/uc?export=view&id=1N2AKP5HcYFk6RksvTYVsDOjdQ1HamWrF"
+    image:
+      "https://lh3.googleusercontent.com/d/1N2AKP5HcYFk6RksvTYVsDOjdQ1HamWrF",
   },
   {
     classId: 1,
     name: "hiro",
-    image: "https://drive.google.com/uc?export=view&id=1OOlKnhppvpJveThcRZ_JSXzOr8OfxdPc"
+    image:
+      "https://lh3.googleusercontent.com/d/1OOlKnhppvpJveThcRZ_JSXzOr8OfxdPc",
   },
   {
     classId: 1,
     name: "Satoya☆",
-    image: "https://drive.google.com/uc?export=view&id=1oIIjnJg3S5-7U3r3eUCcUMB9W5Im8unj"
+    image:
+      "https://lh3.googleusercontent.com/d/1oIIjnJg3S5-7U3r3eUCcUMB9W5Im8unj",
   },
   {
     classId: 1,
     name: "こと",
-    image: "https://drive.google.com/uc?export=view&id=1tKS5MJTepYZHn25o5z2rVpn64l9pKxoD"
+    image:
+      "https://lh3.googleusercontent.com/d/1tKS5MJTepYZHn25o5z2rVpn64l9pKxoD",
   },
   {
     classId: 1,
     name: "ini",
-    image: "https://drive.google.com/uc?export=view&id=1BGPciwzurYaGI1GyvNIWf6gyscPCHkN8"
+    image:
+      "https://lh3.googleusercontent.com/d/1BGPciwzurYaGI1GyvNIWf6gyscPCHkN8",
   },
   {
     classId: 1,
     name: "高橋快成",
-    image: "https://drive.google.com/uc?export=view&id=1-ZPuew9IdqDLiGyy6bjZ48NhQ740NuR9"
+    image:
+      "https://lh3.googleusercontent.com/d/1-ZPuew9IdqDLiGyy6bjZ48NhQ740NuR9",
   },
   {
     classId: 1,
     name: "いおり",
-    image: "https://drive.google.com/uc?export=view&id=12XVeEttF3O60riNms4Dyj390BFqj9e38"
+    image:
+      "https://lh3.googleusercontent.com/d/12XVeEttF3O60riNms4Dyj390BFqj9e38",
   },
   {
     classId: 1,
     name: "みつき",
-    image: "https://drive.google.com/uc?export=view&id=1g3fLcgIkKrZEOLfys_BnfgQe9jsDFIe5"
+    image:
+      "https://lh3.googleusercontent.com/d/1g3fLcgIkKrZEOLfys_BnfgQe9jsDFIe5",
   },
   {
     classId: 1,
     name: "ふたば",
-    image: "https://drive.google.com/uc?export=view&id=1zYM4jcxdijyWiCl7aycIBlIlZRHzkIva"
+    image:
+      "https://lh3.googleusercontent.com/d/1zYM4jcxdijyWiCl7aycIBlIlZRHzkIva",
   },
   {
     classId: 1,
     name: "長谷川拓音",
-    image: "https://drive.google.com/uc?export=view&id=1xCAPEMMGKaB-hmjjgSWCh2m4qIXYdTbO"
+    image:
+      "https://lh3.googleusercontent.com/d/1xCAPEMMGKaB-hmjjgSWCh2m4qIXYdTbO",
   },
   {
     classId: 1,
     name: "原田龍馬",
-    image: "https://drive.google.com/uc?export=view&id=1UaikON0X7JtuRLdmvNhlLcM2TUoFMBbK"
+    image:
+      "https://lh3.googleusercontent.com/d/1UaikON0X7JtuRLdmvNhlLcM2TUoFMBbK",
   },
   {
     classId: 1,
     name: "穴井克和",
-    image: "https://drive.google.com/uc?export=view&id=17n-lrdIqJJOROBnPVx4nkuPbtcpe88Nv"
+    image:
+      "https://lh3.googleusercontent.com/d/17n-lrdIqJJOROBnPVx4nkuPbtcpe88Nv",
   },
   {
     classId: 1,
     name: "平野寿一",
-    image: "https://drive.google.com/uc?export=view&id=1M8CHnKsYgjd8IJoaR5JrilDhI0PzRLMN"
+    image:
+      "https://lh3.googleusercontent.com/d/1M8CHnKsYgjd8IJoaR5JrilDhI0PzRLMN",
   },
   {
     classId: 1,
     name: "高橋ひかり",
-    image: "https://drive.google.com/uc?export=view&id=1Sv3KBGCGo8F1MG8aWhP9FQ_EklhL8mXA"
+    image:
+      "https://lh3.googleusercontent.com/d/1Sv3KBGCGo8F1MG8aWhP9FQ_EklhL8mXA",
   },
   {
     classId: 1,
     name: "久保田あづさ",
-    image: "https://drive.google.com/uc?export=view&id=1APVwlKpbc3lFt0W4Jm2oO83BAlQVkhsG"
+    image:
+      "https://lh3.googleusercontent.com/d/1APVwlKpbc3lFt0W4Jm2oO83BAlQVkhsG",
   },
   {
     classId: 1,
     name: "ﾋﾋﾋ",
-    image: "https://drive.google.com/uc?export=view&id=1J2oJMz7qlDBuLf8f55XVAearFx21y_kE"
+    image:
+      "https://lh3.googleusercontent.com/d/1J2oJMz7qlDBuLf8f55XVAearFx21y_kE",
   },
   {
     classId: 1,
     name: "あやえ",
-    image: "https://drive.google.com/uc?export=view&id=1aloe1zhfMVtDs8rt-RNvrG7PcLSqlKzn"
+    image:
+      "https://lh3.googleusercontent.com/d/1aloe1zhfMVtDs8rt-RNvrG7PcLSqlKzn",
   },
   {
     classId: 1,
     name: "さき",
-    image: "https://drive.google.com/uc?export=view&id=1R40I_PWtQxoUIJKZgF89cis4y52p7Y8X"
+    image:
+      "https://lh3.googleusercontent.com/d/1R40I_PWtQxoUIJKZgF89cis4y52p7Y8X",
   },
   {
     classId: 1,
     name: "武藤環",
-    image: "https://drive.google.com/uc?export=view&id=1crbjMGKpIYMJg--dTO8CQ8fMDgrDmZtj"
+    image:
+      "https://lh3.googleusercontent.com/d/1crbjMGKpIYMJg--dTO8CQ8fMDgrDmZtj",
   },
   {
     classId: 1,
     name: "ゆーき",
-    image: "https://drive.google.com/uc?export=view&id=1uO-oZLCst5TF6ae6vF-kP3iyMBnObEfn"
+    image:
+      "https://lh3.googleusercontent.com/d/1uO-oZLCst5TF6ae6vF-kP3iyMBnObEfn",
   },
   {
     classId: 1,
     name: "おぐ",
-    image: "https://drive.google.com/uc?export=view&id=1ziZyX9WgGgcbvT4yhDkUl73WrhfvkA5_"
+    image:
+      "https://lh3.googleusercontent.com/d/1ziZyX9WgGgcbvT4yhDkUl73WrhfvkA5_",
   },
   {
     classId: 1,
     name: "あるは",
-    image: "https://drive.google.com/uc?export=view&id=1drPb4jvG3JGXIn4cmmPcXqD2ceYjglS3"
+    image:
+      "https://lh3.googleusercontent.com/d/1drPb4jvG3JGXIn4cmmPcXqD2ceYjglS3",
   },
   {
     classId: 1,
     name: "ゆきの",
-    image: "https://drive.google.com/uc?export=view&id=1IYg6-pFKGomV3zi8pE1LKQT9_hponIj-"
+    image:
+      "https://lh3.googleusercontent.com/d/1IYg6-pFKGomV3zi8pE1LKQT9_hponIj-",
   },
   {
     classId: 1,
     name: "ひかり",
-    image: "https://drive.google.com/uc?export=view&id=1BUbBYa3DtJkb7vxcIcXzDRLRrWVV2gDc"
+    image:
+      "https://lh3.googleusercontent.com/d/1BUbBYa3DtJkb7vxcIcXzDRLRrWVV2gDc",
   },
   {
     classId: 1,
     name: "naoki",
-    image: "https://drive.google.com/uc?export=view&id=1UJH0GT3J7IWui5pa3QIRbpDn-RWg4NxR"
+    image:
+      "https://lh3.googleusercontent.com/d/1UJH0GT3J7IWui5pa3QIRbpDn-RWg4NxR",
   },
   {
     classId: 1,
     name: "津布久尚貴",
-    image: "https://drive.google.com/uc?export=view&id=16svFiBpSwofrwROKf_z3N91idnoUlaEb"
+    image:
+      "https://lh3.googleusercontent.com/d/16svFiBpSwofrwROKf_z3N91idnoUlaEb",
   },
   {
     classId: 1,
     name: "Jacksen",
-    image: "https://drive.google.com/uc?export=view&id=1ooof8QKxqordh8PK6bP7ZlPxfe5iA3Ey"
+    image:
+      "https://lh3.googleusercontent.com/d/1ooof8QKxqordh8PK6bP7ZlPxfe5iA3Ey",
   },
   {
     classId: 1,
     name: "YKUZA系イッヌ",
-    image: "https://drive.google.com/uc?export=view&id=1JF7YAwEH82-r3L0GqoncRGVPPy8i88uk"
+    image:
+      "https://lh3.googleusercontent.com/d/1JF7YAwEH82-r3L0GqoncRGVPPy8i88uk",
   },
   {
     classId: 1,
     name: "犬系極道",
-    image: "https://drive.google.com/uc?export=view&id=1783ClE5NOMFfO3vH0qYd9Pdol-ggfVeG"
+    image:
+      "https://lh3.googleusercontent.com/d/1783ClE5NOMFfO3vH0qYd9Pdol-ggfVeG",
   },
   {
     classId: 1,
     name: "もーりもりもりもり",
-    image: "https://drive.google.com/uc?export=view&id=1T07bBeHFhG0RVPJ4ARew5g3frFyVXRF6"
+    image:
+      "https://lh3.googleusercontent.com/d/1T07bBeHFhG0RVPJ4ARew5g3frFyVXRF6",
   },
   {
     classId: 1,
     name: "たちょ",
-    image: "https://drive.google.com/uc?export=view&id=12irtdOVF9RaZR6NRKddYHejQYiWeVsyw"
+    image:
+      "https://lh3.googleusercontent.com/d/12irtdOVF9RaZR6NRKddYHejQYiWeVsyw",
   },
   {
     classId: 2,
     name: "与田亮平",
-    image: "https://drive.google.com/uc?export=view&id=1dLkKe5M1piTIxbFWBD49qCVeBTRe0iqF"
+    image:
+      "https://lh3.googleusercontent.com/d/1dLkKe5M1piTIxbFWBD49qCVeBTRe0iqF",
   },
   {
     classId: 2,
     name: "佐伯綾亮",
-    image: "https://drive.google.com/uc?export=view&id=1EC_SFJ4XVj0Q0SnOcu8t0DxvXPsKSoBv"
+    image:
+      "https://lh3.googleusercontent.com/d/1EC_SFJ4XVj0Q0SnOcu8t0DxvXPsKSoBv",
   },
   {
     classId: 2,
     name: "外﨑未空",
-    image: "https://drive.google.com/uc?export=view&id=1x7MdAauIfDstfBmfSki_JmLq1EcDeW4D"
+    image:
+      "https://lh3.googleusercontent.com/d/1x7MdAauIfDstfBmfSki_JmLq1EcDeW4D",
   },
   {
     classId: 2,
     name: "瀧澤琴萌",
-    image: "https://drive.google.com/uc?export=view&id=1mN0kbiX9f1xFzNsxBV_i5xmEKmB2_8mf"
+    image:
+      "https://lh3.googleusercontent.com/d/1mN0kbiX9f1xFzNsxBV_i5xmEKmB2_8mf",
   },
   {
     classId: 2,
     name: "中城裕之",
-    image: "https://drive.google.com/uc?export=view&id=1S6Kiq3BzLrubzXUMBeso7QuErUSDrdMe"
+    image:
+      "https://lh3.googleusercontent.com/d/1S6Kiq3BzLrubzXUMBeso7QuErUSDrdMe",
   },
   {
     classId: 2,
     name: "あそー",
-    image: "https://drive.google.com/uc?export=view&id=1AwjWFIbSWBBfceC0cCafyd1X_WX_iaf-"
+    image:
+      "https://lh3.googleusercontent.com/d/1AwjWFIbSWBBfceC0cCafyd1X_WX_iaf-",
   },
   {
     classId: 2,
     name: "廣瀬 研人",
-    image: "https://drive.google.com/uc?export=view&id=1K7d0lGb1cA1Wquu1suWQZiwyP0JM2rtT"
+    image:
+      "https://lh3.googleusercontent.com/d/1K7d0lGb1cA1Wquu1suWQZiwyP0JM2rtT",
   },
   {
     classId: 2,
     name: "Yoshii",
-    image: "https://drive.google.com/uc?export=view&id=1qCU3DiNpYUWmti3o9D3BpOKnaRpB3Lyo"
+    image:
+      "https://lh3.googleusercontent.com/d/1qCU3DiNpYUWmti3o9D3BpOKnaRpB3Lyo",
   },
   {
     classId: 2,
     name: "石川ふぁみ",
-    image: "https://drive.google.com/uc?export=view&id=1n-nH-BlsiNYkez2sYhEX3y2iXkC9vIfe"
+    image:
+      "https://lh3.googleusercontent.com/d/1n-nH-BlsiNYkez2sYhEX3y2iXkC9vIfe",
   },
   {
     classId: 2,
     name: "あいか",
-    image: "https://drive.google.com/uc?export=view&id=1gNzSZnsHjE10ERR2vq0wABjs0ep5rZI9"
+    image:
+      "https://lh3.googleusercontent.com/d/1gNzSZnsHjE10ERR2vq0wABjs0ep5rZI9",
   },
   {
     classId: 2,
     name: "ぴっぷ",
-    image: "https://drive.google.com/uc?export=view&id=139dQiLfHQluq0EauINSSiiNoXj_mJbb4"
+    image:
+      "https://lh3.googleusercontent.com/d/139dQiLfHQluq0EauINSSiiNoXj_mJbb4",
   },
   {
     classId: 3,
     name: "伴 匠生",
-    image: "https://drive.google.com/uc?export=view&id=1jbWuRSbNRc0TUUmppfaMOt2DkXLLK3IK"
+    image:
+      "https://lh3.googleusercontent.com/d/1jbWuRSbNRc0TUUmppfaMOt2DkXLLK3IK",
   },
   {
     classId: 3,
     name: "青木 慎太郎",
-    image: "https://drive.google.com/uc?export=view&id=1H2PFBtIKk2LUDQO6-bia6xwRlsbyT0rz"
+    image:
+      "https://lh3.googleusercontent.com/d/1H2PFBtIKk2LUDQO6-bia6xwRlsbyT0rz",
   },
   {
     classId: 3,
     name: "阿部広夢",
-    image: "https://drive.google.com/uc?export=view&id=1Uy6PM3VdXglUy1OdoQuKrgv3qf8U4jn2"
+    image:
+      "https://lh3.googleusercontent.com/d/1Uy6PM3VdXglUy1OdoQuKrgv3qf8U4jn2",
   },
   {
     classId: 3,
     name: "小宮和真",
-    image: "https://drive.google.com/uc?export=view&id=15ZOyibE5zrZF_a3dsBOfUltYhbY1dD3r",
+    image:
+      "https://lh3.googleusercontent.com/d/15ZOyibE5zrZF_a3dsBOfUltYhbY1dD3r",
   },
   {
     classId: 3,
     name: "Rintaro Fukui",
-    image: "https://drive.google.com/uc?export=view&id=1o5T_AFoyLlvPIjPxWX8B3P4zp3gfEFnX"
+    image:
+      "https://lh3.googleusercontent.com/d/1o5T_AFoyLlvPIjPxWX8B3P4zp3gfEFnX",
   },
   {
     classId: 3,
     name: "小林 千沙希",
-    image: "https://drive.google.com/uc?export=view&id=1ihbc5a5fo39nylTWZwCB8Yf-abQi0zgN"
+    image:
+      "https://lh3.googleusercontent.com/d/1ihbc5a5fo39nylTWZwCB8Yf-abQi0zgN",
   },
   {
     classId: 3,
     name: "浅井悠一朗",
-    image: "https://drive.google.com/uc?export=view&id=1w4ThXdiuVGnR5_6tsT4FznDS6ml8pHIC"
-  }
+    image:
+      "https://lh3.googleusercontent.com/d/1w4ThXdiuVGnR5_6tsT4FznDS6ml8pHIC",
+  },
 ];
 
 export default membersData;
