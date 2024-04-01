2730b470835224279c4732c4dc0efffe29f45e16
kakikuu <s2222055@stu.musashino-u.ac.jp>
2024-04-02 00:37:38 +0900

    fix:内容がURLだったので、リンク先に設定して飛べるようにした

--- src/components/works/contents/index.tsx
+++ src/components/works/contents/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'; // FC (FunctionComponent) タイプをインポート
+import { FC } from "react"; // FC (FunctionComponent) タイプをインポート
 import Image from "next/image";
 import styles from "./style.module.css";
 
@@ -9,17 +9,18 @@ type WorksComponentProps = {
   file: any;
 };
 
-
 const DisplayContent: FC<WorksComponentProps> = ({ title, content, file }) => {
   return (
     <div className={styles.works__contents}>
       <div className={styles.works__set}>
-          {file && (
-            <Image src={file} alt={title} className={styles.works__display}/>
-          )}
+        {file && (
+          <Image src={file} alt={title} className={styles.works__display} />
+        )}
         <div className={styles.works__text}>
           <h3>{title}</h3>
-          <p>{content}</p>
+          <a href={content} target="_blank" rel="noopener noreferrer">
+            詳細はこちらから
+          </a>
         </div>
       </div>
     </div>
